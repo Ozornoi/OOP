6de0b6700e2937d4dcd34f2789b4efc4430cfc14
ozorninanton <ozorninanton@mail.ru>
2016-06-13 07:50:44 +0300

    Class Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,26 +26,12 @@ namespace Snake
 
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
 
 
-            }
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 25, 8, '&');
+            line.Drow();
+            verticalLine line1 = new verticalLine(10, 15, 6, '@');
+            line1.Drow();
             Console.ReadLine();
         }
         
---
+++ Snake/Snake/verticalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class verticalLine : Figure
+    {
+        
+
+        public verticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+      
+
+    }
+    }
+
